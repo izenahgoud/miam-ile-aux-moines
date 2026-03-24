d45f37f9082e06e2e68559178cb7e59ee24ab844
izenahgoud <87533505+izenahgoud@users.noreply.github.com>
2026-03-24 18:18:04 +0100

    Update sw.js v1.4

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE = "miam-iam-v1.2";
+const CACHE = "miam-iam-v1.4";
 const ASSETS = [
   "./",
   "./index.html",
 ];
 
 // Tuiles OSM à mettre en cache dynamiquement
-const TILE_CACHE = "miam-tiles-v1";
+const TILE_CACHE = "miam-tiles-v1.4";
 
 self.addEventListener("install", e => {
   e.waitUntil(
