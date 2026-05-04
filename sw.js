63306920ea38dfac837edb54d91804b795e836ec
izenahgoud <87533505+izenahgoud@users.noreply.github.com>
2026-05-04 10:14:18 +0200

    Update sw.js version 1.5

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "miam-iam-v1.4";
+const CACHE = "miam-iam-v1.5";
 const ASSETS = ["./","./index.html","./pois.js","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
