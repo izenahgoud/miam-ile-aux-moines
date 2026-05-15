67b60c67dd07e98e7ce8a7c787b339c7986fde17
izenahgoud <87533505+izenahgoud@users.noreply.github.com>
2026-05-15 18:31:06 +0200

    Update service worker cache version and assets

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "miam-iam-v1.6";
-const ASSETS = ["./","./index.html","./pois.js","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];
+const CACHE = "miam-iam-v1.7";
+const ASSETS = ["./","./index.html","./pois.js","./manifest.json","./icons/icon-192.png","./icons/icon-512.png","./icons/blason.webp"];
 self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener("fetch",e=>{e.respondWith(fetch(e.request).then(r=>{caches.open(CACHE).then(c=>c.put(e.request,r.clone()));return r;}).catch(()=>caches.match(e.request)));});
