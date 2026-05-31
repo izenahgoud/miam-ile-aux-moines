46193bf8bcf9a985d2a1079ebdb496ab7cb6b76b
izenahgoud <87533505+izenahgoud@users.noreply.github.com>
2026-05-31 18:31:00 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,13 +1,11 @@
 const CACHE = "miam-iam-v1.8";
 const ASSETS = [
   "./", "./index.html", "./pois.js", "./evenements.js",
-  "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png", "./icons/blason.webp"
+  "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"
 ];
-
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
-
 self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
@@ -16,21 +14,16 @@ self.addEventListener("activate", e => {
   );
   self.clients.claim();
 });
-
 self.addEventListener("fetch", e => {
-  // Ignorer les URLs non-http (data:, chrome-extension:, etc.)
   if (!e.request.url.startsWith("http")) return;
-
   e.respondWith(
     fetch(e.request).then(r => {
-      // Cloner AVANT de retourner, pas après
       const clone = r.clone();
       caches.open(CACHE).then(c => c.put(e.request, clone));
       return r;
     }).catch(() => caches.match(e.request))
   );
 });
-
 self.addEventListener("message", e => {
   if (e.data && e.data.type === "SKIP_WAITING") self.skipWaiting();
 });
