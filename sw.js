80583e8adc1cafa27344133ea37a87a0b37e655d
izenahgoud <87533505+izenahgoud@users.noreply.github.com>
2026-06-05 07:33:42 +0200

    Update cache version to v1.9.1

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "miam-iam-v1.8";
+const CACHE = "miam-iam-v1.9.1";
 const ASSETS = [
   "./", "./index.html", "./pois.js", "./evenements.js",
   "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"
