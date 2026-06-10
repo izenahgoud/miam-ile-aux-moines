36fc7eaaf2e562141194bd7aecfbfb597c6b1fd2
izenahgoud <87533505+izenahgoud@users.noreply.github.com>
2026-06-10 14:17:47 +0200

    Update evenements.js

--- evenements.js
+++ evenements.js
@@ -17,19 +17,34 @@ const EVENEMENTS = [
     image:       "https://i.ibb.co/nqsCqthc/originemaisons.jpg",
     site:        ""
   },
+   {
+    nom:         "À l'origine de nos maisons",
+    emoji:       "🏠",
+    association: "",
+    date_debut:  "2026-06-14",
+    date_fin:    "2026-06-14",
+    heure:       "17h00 – 18h30",
+    lat:         47.5943,
+    lng:         -2.8502,
+    lieu:        "Salle du Rinville",
+    acces:       "Entrée libre. Places limitées.",
+    description: "",
+    image:       "https://i.ibb.co/nqsCqthc/originemaisons.jpg",
+    site:        ""
+  },
   {
-    nom:         "Tannage des voiles su sinago Crialeis",
-    emoji:       "⛵",
-    association: "Association pour un sinago ilois",
-    date_debut:  "2026-06-13",
-    date_fin:    "2026-06-13",
+    nom:         "",
+    emoji:       "",
+    association: "",
+    date_debut:  "",
+    date_fin:    "2026-09-10",
     heure:       "11h00",
     lat:         47.599056,
     lng:         -2.850298,
-    lieu:        "Petit Pont - Port de l'Ile-aux-Moines",
+    lieu:        "",
     acces:       "Venez nombreux.",
-    description: "Buvette\nRestauration sur place\nAnimation Godille",
-    image:       "https://i.ibb.co/jvHrMrCJ/Tannagevoiles.jpg",
-    site:        "https://www.sinago-crialeis.bzh/"
+    description: "",
+    image:       "",
+    site:        ""
   },
 ];
