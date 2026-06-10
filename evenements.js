70a7641681354c2aa14fede6d2f2f3afce2ced21
izenahgoud <87533505+izenahgoud@users.noreply.github.com>
2026-06-10 19:44:06 +0200

    Replace event details for 'À l'origine de nos maisons'

--- evenements.js
+++ evenements.js
@@ -18,19 +18,19 @@ const EVENEMENTS = [
     site:        ""
   },
    {
-    nom:         "À l'origine de nos maisons",
-    emoji:       "🏠",
-    association: "",
-    date_debut:  "2026-06-14",
-    date_fin:    "2026-06-14",
-    heure:       "17h00 – 18h30",
-    lat:         47.5943,
-    lng:         -2.8502,
-    lieu:        "Salle du Rinville",
-    acces:       "Entrée libre. Places limitées.",
-    description: "",
-    image:       "https://i.ibb.co/nqsCqthc/originemaisons.jpg",
-    site:        ""
+    nom:         "Tannage des voiles du sinago Craleis",
+    emoji:       "",
+    association: "Pour un sinago ilois",
+    date_debut:  "2026-06-13",
+    date_fin:    "2026-06-13",
+    heure:       "11h00 ",
+    lat:         47.599041,
+    lng:         -2.850325,
+    lieu:        "le grand pont - Port du Lerio",
+    acces:       "libre.",
+    description: "Buvette. Restauration sur place. Animation Godille",
+    image:       "https://i.ibb.co/jvHrMrCJ/Tannagevoiles.jpg",
+    site:        "https://www.sinago-crialeis.bzh/"
   },
   {
     nom:         "",
