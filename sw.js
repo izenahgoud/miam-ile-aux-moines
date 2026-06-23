1dd3507ec2307f31b98416059f75f26d709eae2e
izenahgoud <87533505+izenahgoud@users.noreply.github.com>
2026-06-23 17:42:44 +0200

    Remove service worker event listeners

--- sw.js
+++ sw.js
@@ -1,30 +1,11 @@
 const CACHE = "miam-iam-v" + Date.now();
 
 const ASSETS = [
-  "./", "./index.html", "./pois.js", "./evenements.js", "./traces.js",
-  "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"
+  "./",
+  "./index.html",
+  "./pois.js",
+  "./evenements.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
-self.addEventListener("install", e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-});
-self.addEventListener("activate", e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-self.addEventListener("fetch", e => {
-  if (!e.request.url.startsWith("http")) return;
-  e.respondWith(
-    fetch(e.request).then(r => {
-      const clone = r.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
-      return r;
-    }).catch(() => caches.match(e.request))
-  );
-});
-self.addEventListener("message", e => {
-  if (e.data && e.data.type === "SKIP_WAITING") self.skipWaiting();
-});
