fe7cc983e6a9db0123edea73ca7c6b896597c88e
izenahgoud <87533505+izenahgoud@users.noreply.github.com>
2026-06-24 10:20:23 +0200

    Refactor service worker event listeners for clarity

--- sw.js
+++ sw.js
@@ -1,25 +1,41 @@
 const CACHE = "miam-iam-v1.9.1";
+
 const ASSETS = [
-"./", "./index.html", "./pois.js", "./evenements.js",
-"./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"
+  "./",
+  "./index.html",
+  "./pois.js",
+  "./evenements.js",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
+
 self.addEventListener("install", e => {
-e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
 });
+
 self.addEventListener("activate", e => {
-e.waitUntil(
-caches.keys().then(keys =>
-Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-)
-);
-self.clients.claim();
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
 });
+
 self.addEventListener("fetch", e => {
-if (!e.request.url.startsWith("http")) return;
-e.respondWith(
-fetch(e.request).then(r => {
-const clone = r.clone();
-caches.open(CACHE).then(c => c.put(e.request, clone));
-return r;
-}).catch(() => caches.match(e.request))
-);
+  if (!e.request.url.startsWith("http")) return;
+
+  e.respondWith(
+    fetch(e.request)
+      .then(r => {
+        const clone = r.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return r;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
