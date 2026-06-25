fe9c0d5d93d746a21a602ad47b052f18a180ed57
izenahgoud <87533505+izenahgoud@users.noreply.github.com>
2026-06-25 15:39:37 +0200

    Add new events to evenements.js

--- evenements.js
+++ evenements.js
@@ -47,5 +47,51 @@ const EVENEMENTS = [
     "image": "https://i.ibb.co/LhpyZXBV/Anneelisedecre.jpg",
     "site": "",
     "description": "Oeuvres d'Anne-Elise Decré"
-  }
+  },
+   {
+    "nom": "Dîner de l'Ecole St Joseph",
+    "emoji": "🎉",
+    "association": "apel",
+    "date_debut": "2026-07-04",
+    "date_fin": "2026-07-04",
+    "heure": "18h00 - 22h00",
+    "lieu": "Salle du Rinville",
+    "lat": "47.5943",
+    "lng": "-2.8502",
+    "acces": "Sur réservation : Relais des mousquetaires & Boulangerie ",
+    "image": "https://www.mairie-ileauxmoines.fr/wp-content/uploads/2023/11/diner-ecole-1-724x1024.jpg.webp",
+    "site": "",
+    "description": "**Restauration et buvette sur place**\n**Adultes : 15 €** **Enfants: 7 €**\n\n"
+  },
+  {
+    "nom": "3 amis 3 univers",
+    "emoji": "🎨",
+    "association": "",
+    "date_debut": "2026-07-10",
+    "date_fin": "2026-07-14",
+    "heure": "10h00 - 18h00",
+    "lieu": "Salle annexe de la Mairie",
+    "lat": "47.5953",
+    "lng": "-2.8479",
+    "acces": "Accès libre",
+    "image": "https://www.mairie-ileauxmoines.fr/wp-content/uploads/2026/06/kllou.png.webp",
+    "site": "",
+    "description": "**Carol Peregrine** **Mick Abott** **Kilou**"
+  },
+{
+    "nom": "Chanson en chapelle",
+    "emoji": "",
+    "association": "",
+    "date_debut": "2026-07-11",
+    "date_fin": "2026-07-11",
+    "heure": "18h00",
+    "lieu": "Chapelle Notre-Dame d'Espérance",
+    "lat": "47.597743",
+    "lng": "-2.844763",
+    "acces": "Libre participation",
+    "image": "https://public-smartfactory.koredge.fr/sit/client_0000000010/agenda_1h_cIZ4BMEJK_xwFE7n1/original/11-juillet-ile-aux-moines-chanson-en-chapelle.jpg",
+    "site": "https://www.olivierrech.com/",
+    "description": "Olivier RECH Chante la Bretagne : sa mer, son terroir, sa spiritualité et ses légendes. Une voix, une guitare, un programme inédit et original composé de chansons personnelles et de certains standards des répertoires d’Alain Barrière, Hugues Aufray, Michel Tonnerre, François Budet ..."
+  },
+
 ];
