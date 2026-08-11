17a3ba4c42caee08be8301ea27ae2ae602419fa5
izenahgoud <87533505+izenahgoud@users.noreply.github.com>
2026-08-11 20:52:36 +0200

    Fix syntax error in evenements.js

--- evenements.js
+++ evenements.js
@@ -119,7 +119,7 @@ const EVENEMENTS = [
     "lng": -2.845835,
     "lieu": "Salle annexe de la Mairie",
     "acces": "Accès libre. Venez nombreux",
-    "description": ""
+    "description": "",
     "image": "https://www.mairie-ileauxmoines.fr/wp-content/uploads/2023/04/lili.png.webp",
     "site": "https://lilicros.com/"
   },
