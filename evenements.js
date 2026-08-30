cb61869486a8678be39d52aadaf01d5256f8deff
izenahgoud <87533505+izenahgoud@users.noreply.github.com>
2026-08-30 14:44:00 +0200

    Delete event entries from evenements.js
    Removed multiple event entries from the events list.

--- evenements.js
+++ evenements.js
@@ -17,35 +17,6 @@ const EVENEMENTS = [
     "description": "",
     "image": "",
     "site": ""
-  },
-  {
-    "nom": "LA MER NATURE",
-    "emoji": "🎨",
-    "association": "Sylvie BAC",
-    "date_debut": "2026-08-10",
-    "date_fin": "2026-08-30",
-    "heure": "10h00 - 18h00",
-    "lieu": "La Petite Galerie",
-    "lat": 47.6,
-    "lng": -2.851,
-    "acces": "Accès libre. Venez nombreux.",
-    "image": "https://i.ibb.co/wrLmv79w/sylviebac.jpg",
-    "site": "https://sylviebacgallery.fr/",
-    "description": "Ma démarche :\nJe suis entièrement autodidacte, bien qu’ayant par le passé travaillé avec des artistes professionnels, notamment lorsque je vivais en Espagne, à Zaragoza , ou je travaillais comme peintre en décor , pour la réalisation de façades de bars et restaurants."
-  },
-  {
-    "nom": "PASTELS",
-    "emoji": "🎨",
-    "association": "Clotilde Aksin-Frappier",
-    "date_debut": "2026-08-26",
-    "date_fin": "2026-08-30",
-    "heure": "10h00 - 13h00 15h30 - 19h00",
-    "lieu": "Salle annexe de la Mairie",
-    "lat": "47.5970",
-    "lng": "-2.8458",
-    "acces": "Accès libre. Venez nombreux",
-    "image": "https://www.mairie-ileauxmoines.fr/wp-content/uploads/2025/03/Aksin-scaled.jpg.webp",
-    "site": "https://pastelalhuile.fr/co/00accueil.html",
-    "description": "Clotilde Aksin-Frappier est une artiste française dont le travail explore les possibilités expressives et picturales du pastel à l'huile. Par la superposition de couleurs, une approche tactile et une grande sensibilité à la lumière et à l'atmosphère, elle crée des œuvres où paysage, nature morte et formes naturelles se transforment en champs de sensations concentrés. À la galerie Art San, ses œuvres sur supports shikishi japonais prolongent cette pratique dans un format raffiné, en harmonie avec la densité et l'immédiateté de son médium."
   }
+ 
 ];
